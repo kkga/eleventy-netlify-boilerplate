7eac5764e87429e0c5439179980818b3bee9ac62
Gadzhi Kharkharov <kkga@users.noreply.github.com>
2019-09-10 20:34:59 +0300

    cleanup

--- .eleventy.js
+++ .eleventy.js
@@ -68,8 +68,6 @@ module.exports = function(eleventyConfig) {
   // Don't process folders with static assets e.g. images
   eleventyConfig.addPassthroughCopy("static/img");
   eleventyConfig.addPassthroughCopy("_includes/assets/");
-  eleventyConfig.addPassthroughCopy("drafts/assets/");
-  
 
   /* Markdown Plugins */
   let markdownIt = require("markdown-it");
